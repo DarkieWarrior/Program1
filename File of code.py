cfcfef810a0bbcfa236b4b8315973e82a4b72269
Dark_warrior <darkiewar10r@gmail.com>
2021-02-19 23:38:57 +0530

    Made an arithmetic calculator

--- File of code.py
+++ File of code.py
@@ -1,2 +1,12 @@
-print("i hate git")
-print("working now?? i hope.. pls work!!")
+#Takes input from the user in 2 numbers
+
+a=int(input("Give the first number"))
+b=int(input("Give the second number"))
+
+#Gives the numbers after the arithmetic operators have been used:
+
+print("a multiplied by b gives:", a*b)
+print("a added to b gives:", b+a)
+print("b added to a gives:", a=b)
+print("a divided by b gives:", a//b)
+print("the remainder of their division gives:", a*b)
